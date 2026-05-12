efc7a43397b78623cd3624fff846ab617fed4e88
nilimapawase <nilimapawase14@gmail.com>
2026-05-12 12:23:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForJenkinsBuild
+{
+    internal class Program
+    {
+        void Add()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("The Addition is: " + c);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add();
+        }
+    }
+}
